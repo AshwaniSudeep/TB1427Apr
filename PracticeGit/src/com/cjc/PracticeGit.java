be4f487da4ffd441c7369eb95d2b9ab22ffce80d
SUDEEP <SUDEEP@SUDEEP-PC>
2022-04-27 17:12:11 +0530

    By Ash27

---
+++ PracticeGit/src/com/cjc/PracticeGit.java
@@ -0,0 +1,12 @@
+package com.cjc;
+
+public class PracticeGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Practice Git");
+
+	}
+
+}
